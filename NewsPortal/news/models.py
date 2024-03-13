8517f89c60b440903d6e845d4612ab6a9312373a
Sergei Kuryaev <skreason@ya.ru>
2024-03-13 19:19:08 +0500

    Create page

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -1,3 +1,106 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
 
-# Create your models here.
+
+class Author(models.Model):
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        postRat = self.post_set.aggregate(postRating=Sum('rating'))
+        pRating = 0
+        pRating += postRat.get('postRating')
+
+        commentRat = self.author_user.comment_set.aggregate(commentRating=Sum('rating'))
+        cRat = 0
+        cRat += commentRat.get('commentRating')
+
+        self.ratingAuthor = pRating * 3 + cRat
+        self.save()
+
+    def __str__(self):
+        return self.author_user.username.title()
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name.title()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+
+    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def preview(self):
+        return f'{self.text[0: 124]}...'
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+
+class PostCategory(models.Model):
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.categoryThrough.name.title()}/Пост {self.postThrough.id}'
+
+    class Meta:
+        verbose_name = 'Категория поста'
+        verbose_name_plural = 'Категории постов'
+
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'news.html'
+    context_object_name = 'Новости'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'Новость'
